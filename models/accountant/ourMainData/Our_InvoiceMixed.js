1b63e16143333322a4c96053e69b38dd618295fc
sershishkov <ser.shishkov@gmail.com>
2020-03-14 10:19:31 +0200

    back started mainData in accountant

---
+++ models/accountant/ourMainData/Our_InvoiceMixed.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const InvoiceProduct = new mongoose.Schema({
+  invoceProductNumber: {
+    type: String,
+    required: [true, 'Введите номер счета'],
+    unique: true
+  },
+  invoceProductDate: {
+    type: Date,
+    required: [true, 'Введите дату счета'],
+    default: Date.now
+  },
+  ourFirm: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'OurFirm',
+    required: [true, 'Выберите фирму Исполнителя']
+  },
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Client',
+    required: [true, 'Выберите фирму Заказчика']
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: [true, 'Выберите товар']
+      },
+      amount: {
+        type: Number,
+        required: [true, 'Введите количество']
+      },
+      price: {
+        type: Number,
+        required: [true, 'Введите цену']
+      }
+    }
+  ],
+  serviceJobs: [
+    {
+      serviceJob: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ServiceJob',
+        required: [true, 'Выберите услугу']
+      },
+      amount: {
+        type: Number,
+        required: [true, 'Введите количество']
+      },
+      price: {
+        type: Number,
+        required: [true, 'Введите цену']
+      }
+    }
+  ]
+});
+
+module.exports = mongoose.model('InvoiceProduct', InvoiceProduct);
