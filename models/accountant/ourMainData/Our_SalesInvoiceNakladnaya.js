3f1dd9395e980e17d050239233f59f2bbaace055
sershishkov <ser.shishkov@gmail.com>
2020-03-26 08:03:16 +0200

    Our_SalesInvoiceNakladnaya_Schema virtual field

--- models/accountant/ourMainData/Our_SalesInvoiceNakladnaya.js
+++ models/accountant/ourMainData/Our_SalesInvoiceNakladnaya.js
@@ -1,72 +1,85 @@
 const mongoose = require('mongoose');
 
-const Our_SalesInvoiceNakladnaya_Schema = new mongoose.Schema({
-  naklNumber: {
-    type: String,
-    required: [true, 'Введите номер накладной'],
-    unique: true
-  },
+const Our_SalesInvoiceNakladnaya_Schema = new mongoose.Schema(
+  {
+    naklNumber: {
+      type: String,
+      required: [true, 'Введите номер накладной'],
+      unique: true
+    },
 
-  naclDate: {
-    type: Date,
-    // required: [true, 'Введите дату накладной'],
-    default: Date.now
-  },
-  ourFirm: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'OurFirm',
-    required: [true, 'Выберите фирму Исполнителя']
-  },
-  client: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Client',
-    required: [true, 'Выберите фирму Заказчика']
-  },
-  products: [
-    {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product',
-        required: [true, 'Выберите товар']
-      },
-      amount: {
-        type: Number,
-        required: [true, 'Введите количество']
-      },
-      price: {
-        type: Number,
-        required: [true, 'Введите цену']
+    naclDate: {
+      type: Date,
+      // required: [true, 'Введите дату накладной'],
+      default: Date.now
+    },
+    ourFirm: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'OurFirm',
+      required: [true, 'Выберите фирму Исполнителя']
+    },
+    client: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Client',
+      required: [true, 'Выберите фирму Заказчика']
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: [true, 'Выберите товар']
+        },
+        amount: {
+          type: Number,
+          required: [true, 'Введите количество']
+        },
+        price: {
+          type: Number,
+          required: [true, 'Введите цену']
+        }
       }
+    ],
+    // sum: {
+    //   type: Number
+    //   // required: true
+    // },
+    active: {
+      type: Boolean,
+      // required: true,
+      default: true
+    },
+    cashPayment: {
+      type: Boolean,
+      // required: true,
+      default: false
     }
-  ],
-  sum: {
-    type: Number
-    // required: true
   },
-  active: {
-    type: Boolean,
-    // required: true,
-    default: true
-  },
-  cashPayment: {
-    type: Boolean,
-    // required: true,
-    default: false
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   }
-});
+);
 
-Our_SalesInvoiceNakladnaya_Schema.pre('save', async function(next) {
-  if (!this.products) {
-    next();
-  }
+Our_SalesInvoiceNakladnaya_Schema.virtual('sum').get(function() {
   let sumOfProduct = 0;
   this.products.forEach(item => {
     sumOfProduct += item.amount * item.price;
   });
-  this.sum = sumOfProduct;
-
-  next();
+  return sumOfProduct;
 });
+
+// Our_SalesInvoiceNakladnaya_Schema.pre('save', async function(next) {
+//   if (!this.products) {
+//     next();
+//   }
+//   let sumOfProduct = 0;
+//   this.products.forEach(item => {
+//     sumOfProduct += item.amount * item.price;
+//   });
+//   this.sum = sumOfProduct;
+//   next();
+// });
 
 module.exports = mongoose.model(
   'Our_SalesInvoiceNakladnaya',
